0c5ee3a0c6da83f1a3715d209944f4d35acea171
Javierjmc <javierjosemartinezcedeno@gmail.com>
2023-11-26 17:59:02 -0400

    section agregada

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-let iconoBurger=document.querySelector('#icono-hamburguesa');
-let menu=document.querySelector('#menu');
+const iconoBurger=document.querySelector('#icono-hamburguesa');
+const menu=document.querySelector('#menu');
+const fondo=document.querySelector('#fondo');
+const textoModo=document.querySelector('#textoModo')
 
 
 iconoBurger.addEventListener('click', ()=>{
     menu.classList.toggle('translate-y-[0]')
 }
 );
+
+fondo.addEventListener('click', ()=>{
+    if (textoModo.textContent='Modo dia') {
+        textoModo.textContent='Modo noche';
+    }else{
+        textoModo.textContent='Modo noche';
+    }    
+}
+);
+
 
 
